19f7d65db2afa047504abc7144f717ee898578ba
Joanne Huang <joanne2713184@gmail.com>
2024-09-26 01:05:59 +0800

    Add sam_get_mask.py and detect_test.py

---
+++ detect_test.py
@@ -0,0 +1,139 @@
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLO root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, smart_inference_mode
+
+class detect():
+
+    def run(self, 
+            source,
+    weights='yolov9/yolov9-s-converted.pt',  # model path or triton URL
+    # source='yolov9/data/images/horses.jpg',  # file/dir/URL/glob/screen/0(webcam)
+    data='./data/coco.yaml',  # dataset.yaml path
+    imgsz=(640, 640),  # inference size (height, width)
+    conf_thres=0.25,  # confidence threshold
+    iou_thres=0.45,  # NMS IOU threshold
+    max_det=1000,  # maximum detections per image
+    device='0',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    view_img=False,  # show results
+    save_txt=False,  # save results to *.txt
+    save_conf=False,  # save confidences in --save-txt labels
+    save_crop=False,  # save cropped prediction boxes
+    nosave=False,  # do not save images/videos
+    classes=None,  # filter by class: --class 0, or --class 0 2 3
+    agnostic_nms=False,  # class-agnostic NMS
+    augment=False,  # augmented inference
+    visualize=False,  # visualize features
+    update=False,  # update all models
+    project='./runs/detect',  # save results to project/name
+    name='exp',  # save results to project/name
+    exist_ok=False,  # existing project/name ok, do not increment
+    line_thickness=3,  # bounding box thickness (pixels)
+    hide_labels=False,  # hide labels
+    hide_conf=False,  # hide confidences
+    half=False,  # use FP16 half-precision inference
+    dnn=False,  # use OpenCV DNN for ONNX inference
+    vid_stride=1,  # video frame-rate stride
+    ):
+        
+        source = str(source)
+        save_img = not nosave and not source.endswith('.txt')  # save inference images
+        is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+        is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+        webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
+        screenshot = source.lower().startswith('screen')
+        if is_url and is_file:
+            source = check_file(source)  # download
+
+        # Load model
+        device = select_device(device)
+        model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+        stride, names, pt = model.stride, model.names, model.pt
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+        # Dataloader
+        bs = 1  # batch_size
+        if webcam:
+            view_img = check_imshow(warn=True)
+            dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+            bs = len(dataset)
+        elif screenshot:
+            dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
+        else:
+            dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        vid_path, vid_writer = [None] * bs, [None] * bs
+
+        # Run inference
+        model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+        seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+        for path, im, im0s, vid_cap, s in dataset:
+            with dt[0]:
+                im = torch.from_numpy(im).to(model.device)
+                im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+                im /= 255  # 0 - 255 to 0.0 - 1.0
+                if len(im.shape) == 3:
+                    im = im[None]  # expand for batch dim
+
+            # Inference
+            with dt[1]:
+                # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+                pred = model(im, augment=augment, visualize=visualize)
+
+            # NMS
+            with dt[2]:
+                pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+                if webcam:  # batch_size >= 1
+                    p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                    s += f'{i}: '
+                else:
+                    p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+                p = Path(p)  # to Path
+                # save_path = str(save_dir / p.name)  # im.jpg
+                # txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+                s += '%gx%g ' % im.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+                    # print(det[:, :4])
+
+                    # Print results
+                    for c in det[:, 5].unique():
+                        n = (det[:, 5] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                        # print(names[int(c)])
+
+            # Print time (inference-only)
+            # LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+        # Print results
+        t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
+        # LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+
+        if update:
+            strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+        
+        return names[int(c)], det[:, :4]
---
+++ sam_get_mask.py
@@ -0,0 +1,149 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import cv2
+from segment_anything import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
+import os
+import sys
+yolo_path = 'C:/Users/joann/Desktop/Joanne_Project/Image_Recognition_Side_Project'
+sys.path.append(yolo_path)
+from yolov9.detect_test import detect
+sys.path.remove(yolo_path)
+yolo_detect = detect()
+
+class mask():
+
+    def show_mask(self, mask, ax, random_color=False):
+        if random_color:
+            color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+        else:
+            color = np.array([30/255, 144/255, 255/255, 0.6])
+        h, w = mask.shape[-2:]
+        mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+        ax.imshow(mask_image)
+        
+    def show_points(self, coords, labels, ax, marker_size=375):
+        pos_points = coords[labels==1]
+        neg_points = coords[labels==0]
+        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+        
+    def show_box(self, box, ax):
+        x0, y0 = box[0], box[1]
+        w, h = box[2] - box[0], box[3] - box[1]
+        ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+    def show_res(self, masks, scores, input_point, input_label, input_box, filename, image):
+        for i, (mask, score) in enumerate(zip(masks, scores)):
+            plt.figure(figsize=(10,10))
+            plt.imshow(image)
+            self.show_mask(mask, plt.gca())
+            if input_box is not None:
+                box = input_box[i]
+                self.show_box(box, plt.gca())
+            if (input_point is not None) and (input_label is not None): 
+                self.show_points(input_point, input_label, plt.gca())
+            
+            print(f"Score: {score:.3f}")
+            plt.axis('off')
+            plt.savefig(filename+'_'+str(i)+'.png',bbox_inches='tight',pad_inches=-0.1)
+            plt.close()
+
+    def show_res_multi(self, masks, scores, input_point, input_label, input_box, filename, image):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        for mask in masks:
+            self.show_mask(mask, plt.gca(), random_color=True)
+        for box in input_box:
+            self.show_box(box, plt.gca())
+        for score in scores:
+            print(f"Score: {score:.3f}")
+        plt.axis('off')
+        plt.savefig(filename +'.png',bbox_inches='tight',pad_inches=-0.1)
+        plt.close()
+
+    def save_idivisual_imag(self, image, mask) :
+        # Check image and mask is None or not
+        if image is None:
+            raise ValueError("Error: 'image' is None. Please check if the image is correctly loaded.")
+        if mask is None:
+            raise ValueError("Error: 'mask' is None. Please check if the mask is correctly generated or passed.")
+        
+        # 如果 masks 是三维的（即包含多个 mask），选择一个或合并所有 mask
+        if len(mask.shape) == 3:
+            # 使用合并 mask 的方法
+            mask = np.max(mask, axis=0)  # 合并所有的 mask
+        
+        masks = (mask.astype(np.uint8)) * 255
+        # masks = np.squeeze(masks)
+        if masks.shape[:2] != image.shape[:2]:
+            print(f"Resizing mask from {masks.shape[:2]} to {image.shape[:2]}")
+            masks = cv2.resize(masks, (image.shape[1], image.shape[0]))
+
+        segmented_object = cv2.bitwise_and(image, image, mask=masks)
+        plt.imshow(segmented_object)
+        plt.axis('off')
+        # plt.savefig(filename+'_'+str(i)+'.png',bbox_inches='tight',pad_inches=-0.1)
+        plt.close()
+        return segmented_object
+
+    def get_mask(self, image_path):
+        sam_checkpoint = "sam_hq/pretrained_checkpoint/sam_hq_vit_b.pth"
+        model_type = "vit_b"
+        device = "cuda"
+        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        sam.to(device=device)
+        predictor = SamPredictor(sam)
+
+        # hq_token_only: False means use hq output to correct SAM output. 
+        #                True means use hq output only. 
+        #                Default: False
+        hq_token_only = False 
+        # To achieve best visualization effect, for images contain multiple objects (like typical coco images), we suggest to set hq_token_only=False
+        # For images contain single object, we suggest to set hq_token_only = True
+        # For quantiative evaluation on COCO/YTVOS/DAVIS/UVO/LVIS etc., we set hq_token_only = False
+
+        image = cv2.imread(image_path)
+        print(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        predictor.set_image(image)
+
+        input_box_1 = yolo_detect.run(image_path)[1]
+        input_box_1 = input_box_1.detach()
+
+        
+        # multi box input
+        # input_box = torch.tensor([[1, 192, 316, 411], [434, 212, 596, 348]],device=predictor.device)
+        transformed_box = predictor.transform.apply_boxes_torch(input_box_1, image.shape[:2])
+        input_point, input_label = None, None
+
+        batch_box = False if input_box_1 is None else len(input_box_1)>1 
+        result_path = 'sam_hq/demo/hq_sam_result/'
+        indivisual_result_path = 'sam_hq/demo/hq_sam_indivisual_result/'
+        os.makedirs(result_path, exist_ok=True)
+        os.makedirs(indivisual_result_path, exist_ok=True)
+
+        if not batch_box: 
+            masks, scores, logits = predictor.predict(
+                point_coords=input_point,
+                point_labels=input_label,
+                box = input_box_1,
+                multimask_output=False,
+                hq_token_only=hq_token_only, 
+            )
+            # self.show_res(masks,scores,input_point, input_label, input_box_1, result_path + 'example'+str(i), image)
+            return self.save_idivisual_imag(image, masks)
+            
+        else:
+            masks, scores, logits = predictor.predict_torch(
+                point_coords=input_point,
+                point_labels=input_label,
+                boxes=transformed_box,
+                multimask_output=False,
+                hq_token_only=hq_token_only,
+            )
+            masks = masks.squeeze(1).cpu().numpy()
+            scores = scores.squeeze(1).cpu().numpy()
+            input_box = input_box_1.cpu().numpy()
+            # self.show_res_multi(masks, scores, input_point, input_label, input_box, result_path + 'example'+str(i), image)
+            return self.save_idivisual_imag(image, masks)
